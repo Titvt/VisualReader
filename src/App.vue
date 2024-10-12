34ec71ded260ab13b55a4b7bdae7777f86178594
Titvt <tqydyqt@126.com>
2024-10-12 17:39:56 +0800

    add virtual scroller

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script setup>
 import { ref } from 'vue'
 
-const text = ref('Visual Reader')
+const items = ref(Array.from({ length: 1000000 }).map((_, i) => i));
 </script>
 
 <template>
-    <Button>{{ text }}</Button>
+    <VirtualScroller :items="items" :item-size="24" class="container">
+        <template v-slot:item="{ item, options }">
+            <div class="item">
+                {{ options.index }} {{ item }}
+            </div>
+        </template>
+    </VirtualScroller>
 </template>
 
 <style>
 body {
     margin: 0;
+}
+
+.container {
+    width: 100vw;
+    height: calc(100vh - 60px);
+}
+
+.item {
+    width: calc(100% - 24px);
+    margin: 12px;
 }
 </style>
